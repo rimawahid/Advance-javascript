3751a85cbfb591148691b2d25ae418e135677099
Rima Akter <rimawahid3@gmail.com>
2021-02-02 00:15:48 +0600

    null vs undefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,18 @@
+let number;
+function add (num1, num2) {
+    console.log (num1 +num2);
+    return
+}
+console.log(number);
+function add (num1, num2){
+    console.log(num1 +num2);
+}
+
+const person ={name: 'Shanto', phone: '01515112233'};
+console.log(person.gf);
+
+let ages = [2,4,6,5];
+console.log(ages[4])
+
+const result =add(13)
+console.log(result);
