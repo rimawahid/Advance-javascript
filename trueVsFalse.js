eb13484af2d13dd556dd6c2dfc2cf6590fcec190
Rima Akter <rimawahid3@gmail.com>
2021-02-01 23:36:54 +0600

    truthy vs falsy

---
+++ trueVsFalse.js
@@ -0,0 +1,19 @@
+//falsy 
+//0
+//undefined
+//NaN
+//null
+//""
+
+//truthy
+//'0'
+//' '
+//[]
+// const age = "0";
+
+let name= []
+if(name){
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+}
